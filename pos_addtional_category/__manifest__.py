2a31a862480d82df970ad456a33eac2c404c8917
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-01-28 23:26:00 +0100

    1

--- pos_addtional_category/__manifest__.py
+++ pos_addtional_category/__manifest__.py
@@ -3,16 +3,13 @@
     "name": """Multi categories in POS""",
     "summary": """Specify as many categories for a product as you need""",
     "category": "Point of Sale",
-    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=13.0",
     "images": [],
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.0",
     "application": False,
-    "author": "IT-Projects LLC, Pavel Romanchenko",
-    "support": "pos@it-projects.info",
-    "website": "https://www.odoo.com/apps/modules/13.0/pos_category_multi/",
+    "author": "Serincloud SL",
+    "support": "soporte@ingenieriacloud.com",
+    "website": "https://www.ingenieriacloud.com",
     "license": "Other OSI approved licence",  # MIT
-    "price": 15.00,
-    "currency": "EUR",
     "depends": ["point_of_sale"],
     "external_dependencies": {"python": [], "bin": []},
     "data": ["template.xml", "view.xml"],
@@ -24,14 +21,4 @@
     "uninstall_hook": None,
     "auto_install": False,
     "installable": True,
-    # "demo_title": "{MODULE_NAME}",
-    # "demo_addons": [
-    # ],
-    # "demo_addons_hidden": [
-    # ],
-    # "demo_url": "DEMO-URL",
-    # "demo_summary": "{SHORT_DESCRIPTION_OF_THE_MODULE}",
-    # "demo_images": [
-    #    "images/MAIN_IMAGE",
-    # ]
 }
