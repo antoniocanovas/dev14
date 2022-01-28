777d05acea5befdc2784089aa70204829d91bc79
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-01-28 23:36:04 +0100

    3

--- pos_addtional_category/models/product_categ_rels.py
+++ pos_addtional_category/models/product_categ_rels.py
@@ -4,11 +4,11 @@ class ProductCategRel(models.Model):
     _name = 'pos.product.category.rel'
     _description = 'Pos categories rel'
 
-    product_tmpl_id = fields.Many2one(
-        'product.template'
-    )
     pos_category_id = fields.Many2one(
        'pos.category'
+    )
+    product_tmpl_id = fields.Many2one(
+        'product.template'
     )
 
 
@@ -16,10 +16,10 @@ class ProductCategAddRel(models.Model):
     _name = 'pos.product.category.add.rel'
     _description = 'Pos categories add rel'
 
-    product_tmpl_id = fields.Many2one(
-        'product.template'
-    )
     pos_category_id = fields.Many2one(
        'pos.category'
+    )
+    product_tmpl_id = fields.Many2one(
+        'product.template'
     )
 
