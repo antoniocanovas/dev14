da2d85e754fe514e4b84c480b3c6c1fce315f94b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-02-27 03:56:38 +0100

    v2

--- hr_timesheet_work_repair/models/__init__.py
+++ hr_timesheet_work_repair/models/__init__.py
@@ -1,10 +1,8 @@
 #from . import timesheet_work
 from . import work_sheet
+from . import account_analytic_line
 #from . import stock_move
 #from . import repair_line
-#from . import sale_order_line
-#from . import sale_order
-#from . import working_type
 
 
 
--- hr_timesheet_work_repair/models/account_analytic_line.py
+++ hr_timesheet_work_repair/models/account_analytic_line.py
@@ -7,8 +7,4 @@ _logger = logging.getLogger(__name__)
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    work_sheet_id = fields.Many2one('work.sheet', store=True)
-    work_sheet_so_line_id = fields.Many2one('sale.order.line', store=True)
-
-    type_id = fields.Many2one('working.type', 'Type', store=True)
-    sale_id = fields.Many2one('sale.order', 'Sale order', store=True)
+    repair_id = fields.Many2one('repair.order', 'Repair', store=True)
