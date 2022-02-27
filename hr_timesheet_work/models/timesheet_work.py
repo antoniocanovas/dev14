0a7f6c3d6b61c0ba3845326771db6df0848e1567
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-02-27 02:52:46 +0100

    v2

--- hr_timesheet_work/models/timesheet_work.py
+++ hr_timesheet_work/models/timesheet_work.py
@@ -5,18 +5,18 @@ _logger = logging.getLogger(__name__)
 
 
 TYPES = [
-    ('project', 'Project'),
+    ('project', 'Project')
 ]
 
 
-class IsetsTypes(models.Model):
+class TimesheetWork(models.Model):
     _name = 'timesheet.work'
     _description = 'Timesheet Work'
 
     name = fields.Char('Name', required=True)
     active = fields.Boolean(default=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
-    type = fields.Selection([('project', 'Project')], required=True, string='Type', default='project')
+    type = fields.Selection(TYPES, required=True, string='Type', default='project')
     project_id = fields.Many2one('project.project')
     set_start_stop = fields.Boolean('Set start & stop time')
 
