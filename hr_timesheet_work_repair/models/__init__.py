d3704bd13bfdd871f2e21f23abc0697608e82bde
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-02-27 13:52:59 +0100

    v2

--- hr_timesheet_work_repair/models/__init__.py
+++ hr_timesheet_work_repair/models/__init__.py
@@ -1,5 +1,5 @@
-#from . import timesheet_work
-from . import work_sheet
+from . import timesheet_work
+#from . import work_sheet
 from . import account_analytic_line
 #from . import stock_move
 #from . import repair_line
