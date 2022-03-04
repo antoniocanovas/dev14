1479608a1020678e4256ddd172ceaaa5b3bd3746
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-04 17:33:17 +0100

    hrtmsh0

---
+++ hr_timesheet_work/models/account_analytic_line.py
@@ -0,0 +1,15 @@
+from odoo import _, api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    work_sheet_id = fields.Many2one('work.sheet', store=True)
+    type_id = fields.Many2one('project.time.type', 'Type', store=True)
+
+    # Para revisar si en versión 03/22 hace falta:
+    work_sheet_so_line_id = fields.Many2one('sale.order.line', store=True)
+    sale_id = fields.Many2one('sale.order', 'Sale order', store=True)
