ad4c7e17528f1fb86198b16d6da890b3aace0740
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-05 11:19:41 +0100

    hrtmsh3

--- hr_timesheet_work/models/account_analytic_tag.py
+++ hr_timesheet_work/models/account_analytic_tag.py
@@ -7,4 +7,4 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     _inherit = 'account.analytic.tag'
 
-    timesheet_hidden = fields.Boolean(string='In timesheets', help='If true, this tag will be hidden in timesheets')
+    timesheet_hidden = fields.Boolean(string='Hidden in Timesheet', help='If true, this tag will be hidden in lot work timesheets creation')
