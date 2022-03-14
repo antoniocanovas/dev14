cbe206aa11ab1af9a2a35e97922a19f26b44cd93
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-14 03:10:21 +0100

    gappro_v1

--- project_procedure/models/project_procedure_gap.py
+++ project_procedure/models/project_procedure_gap.py
@@ -21,7 +21,11 @@ class ProjectProcedure(models.Model):
                              ('picking','Albarán'),('invoice','Factura')], required=True)
 
     lead_ids = fields.Many2many('crm.lead')
-    sale_ids = fields.Many2many('sale.order')
+    sale_ids = fields.Many2many(comodel_name='sale.order',
+                                relation='project_gap_rel',
+                                column1='gap_id',
+                                column2='sale_id',
+                                ))
     purchase_ids = fields.Many2many('purchase.order')
     task_ids = fields.Many2many('project.task')
     picking_ids = fields.Many2many('stock.picking')
