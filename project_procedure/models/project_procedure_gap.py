705a4581d873e2da9a3289401e0269519ad01e94
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-14 02:48:27 +0100

    gappro_v1

--- project_procedure/models/project_procedure_gap.py
+++ project_procedure/models/project_procedure_gap.py
@@ -10,7 +10,7 @@ class ProjectProcedure(models.Model):
     _description = 'Etapas en procedimiento de proyectos'
 
     name = fields.Char(string='Nombre', required=True)
-    priority = fields.inteber(string='Prioridad')
+    priority = fields.integer(string='Prioridad')
     user_id = fields.Many2one('res.users', string='Responsable', required=True, store=True)
     date_limit = fields.Date(string='Fecha límite')
     project_id = fields.Many2one('project.project', string='Proyecto')
