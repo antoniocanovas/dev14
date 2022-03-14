c87091a7bd04d77f240cf23a494a5f85805ee1df
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-03-14 17:23:30 +0100

    phases_v1

--- project_phase/__manifest__.py
+++ project_phase/__manifest__.py
@@ -15,10 +15,10 @@
         'base_automation',
     ],
     'data': [
-#        'views/views.xml',
-#        'views/views_menu.xml',
         'security/ir.model.access.csv',
         'views/project_view.xml',
+        'views/project_phase_view.xml',
+#        'views/views_menu.xml',
     ],
     'installable':True,
 }
