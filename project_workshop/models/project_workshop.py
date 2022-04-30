40d9eb111816d041647d9de004f46b714110285f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-04-30 03:17:04 +0200

    project_workshop_0

--- project_workshop/models/project_workshop.py
+++ project_workshop/models/project_workshop.py
@@ -12,7 +12,7 @@ class ProjectWorkshop(models.Model):
     _description = ""
 
     name = fields.Char('Asunto', required=True, store=True)
-    description = fields.Html('Notas')
+    description = fields.Text('Notas')
     project_id = fields.Many2one('project.project', string='Vehículo')
     project_partner_id = fields.Many2one('res.partner', related='project_id.partner_id', string='Cliente')
     is_new = fields.Boolean("Nuevo")
@@ -25,4 +25,21 @@ class ProjectWorkshop(models.Model):
 
     def create_workshop_task(self):
         for record in self:
-            a = 1
+            project = record.project_id
+            if (record.is_new == True) and (record.project_id.id == False):
+                # Crear Proyecto:
+                name = "[" + record.license + "] " + record.model
+                project = self.env['project.project'].create(
+                    {'name': name, 'partner_id': record.partner_id.id, 'allow_timesheets': True,
+                     'allow_billable': True, })
+                record.write({'project_id': project.id, 'is_new': False})
+
+            # Crear pedido de venta con línea de servicio y tarea asociada a la línea:
+            if (record.sale_line_id.id == False):
+                saleorder = self.env['sale.order'].create({'partner_id': project.partner_id.id})
+                saleline = self.env['sale.order.line'].create(
+                    {'order_id': saleorder.id, 'product_id': project.timesheet_product_id.id})
+                record['sale_line_id'] = saleline.id
+                task = self.env['project.task'].create(
+                    {'project_id': project.id, 'name': record.name, 'description': record.description,
+                     'partner_id': project.partner_id.id, 'sale_line_id': saleline.id})
