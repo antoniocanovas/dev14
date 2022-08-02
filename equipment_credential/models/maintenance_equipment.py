e2dea0797e9e77d7cd8aedcb3c78d5573ee619bf
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-08-02 12:17:54 +0200

    equipment_credential rev

--- equipment_credential/models/maintenance_equipment.py
+++ equipment_credential/models/maintenance_equipment.py
@@ -11,10 +11,14 @@ class MaintenanceEquipmentCredentials(models.Model):
     pathway_id = fields.Many2one('equipment.service', name="Pathway",
                                  domain=[('type_id.is_pathway','=',True)])
     credentials_ids = fields.One2many('partner.credentials', 'equipment_id')
+    service_ids = fields.One2many('equipment.service', 'equipment_id')
 
     def _get_credentials_equipment(self):
         results = self.env['partner.credentials'].search([('equipment_id', '=', self.id)])
         self.credential_count = len(results)
-
     credential_count = fields.Integer('Credentials', compute=_get_credentials_equipment, store=False)
 
+    def _get_services_equipment(self):
+        results = self.env['equipment.service'].search([('equipment_id', '=', self.id)])
+        self.services_count = len(results)
+    services_count = fields.Integer('Credentials', compute=_get_credentials_equipment, store=False)
