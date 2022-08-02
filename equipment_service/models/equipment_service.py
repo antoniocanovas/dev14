9682a737518c4ab7769dbb31686ca4501cd49cb2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-08-03 01:33:53 +0200

    equipment_credential v4

--- equipment_service/models/equipment_service.py
+++ equipment_service/models/equipment_service.py
@@ -6,11 +6,14 @@ from odoo import fields, models, api
 
 class EquipmentService(models.Model):
     _name = 'equipment.service'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Equipment Service'
 
     name = fields.Char('Name')
     type_id = fields.Many2one('equipment.service.type', string="Type")
     equipment_id = fields.Many2one('maintenance.equipment', string="Equipment")
+    note = fields.Text('Description')
+    active = fields.Boolean('Active')
 
 
 
