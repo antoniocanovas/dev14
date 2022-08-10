a8f0f4149a04bcfecec4ec014053efa4e77cf1cc
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-08-10 10:16:52 +0200

    group prices

---
+++ product_group_price/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Group see prices",
+    'summary': """
+        Hide prices to users not in sales, purchases and invoicing.
+        """,
+    'author': "Serincloud SL",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '14.0.1.0.0',
+    'depends': [
+        'product',
+    ],
+    'data': [
+#        'views/views.xml',
+        'security/res_group.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
