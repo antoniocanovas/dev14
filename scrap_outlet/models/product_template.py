9113ba76df9e05058b786f92d4123a3eb1d3a603
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-08-27 01:16:00 +0200

    scrap_outlet

--- scrap_outlet/models/product_template.py
+++ scrap_outlet/models/product_template.py
@@ -3,7 +3,7 @@ from odoo import _, api, fields, models
 import logging
 _logger = logging.getLogger(__name__)
 
-VEHICLE_STATE = [
+OUTLET_STATE = [
     ('used', 'Usado'),
     ('new', 'Nuevo'),
     ('km0', 'Kilómetro 0'),
@@ -17,11 +17,11 @@ class ProductTemplate(models.Model):
     outlet_date = fields.Date(string="Date")
     outlet_model_id = fields.Many2one("fleet.vehicle.model", string="Model")
     outlet_brand_id = fields.Many2one("fleet.vehicle.model.brand",
-                                       related="vehicle_model_id.brand_id",
+                                       related="outlet_model_id.brand_id",
                                        string="Brand")
     outlet_category_id = fields.Many2one("fleet.vehicle.category", string="Category")
 
-    outlet_state = fields.Selection(selection=VEHICLE_STATE, string="Estado")
+    outlet_state = fields.Selection(selection=OUTLET_STATE, string="Estado")
     outlet_license = fields.Char('Matrícula')
 
     @api.depends('unbuild_type')
