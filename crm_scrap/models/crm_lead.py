3653a6c1adc6eb71bb217baa0c69717a129dea69
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-11 17:02:37 +0200

    crm_scrap v0

--- crm_scrap/models/crm_lead.py
+++ crm_scrap/models/crm_lead.py
@@ -7,11 +7,3 @@ class CrmLead(models.Model):
     task_ids = fields.One2many('project.task', 'lead_id', store=True)
     scrap_project_id = fields.Many2one('project.project', store=False, related='company_id.scrap_project_id')
     scrap_user_id = fields.Many2one('res.users', store=False, related='company_id.scrap_user_id')
-
-    @api.depends('create_date')
-    def get_lead_self(self):
-        for record in self:
-            lead = self.env['crm.lead'].search([('id', '=', record.id)])
-            record.self = lead.id
-    self = fields.Many2one('crm.lead', string="Self", store=True, compute="get_lead_self")
-
