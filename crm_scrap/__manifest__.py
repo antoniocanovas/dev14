ee33be099ec6ac789699b20c0f401ad4470fa93e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-11 09:58:04 +0200

    crm_scrap v0

--- crm_scrap/__manifest__.py
+++ crm_scrap/__manifest__.py
@@ -14,6 +14,7 @@ SCRAP demand integration in CRM
     'data': [
         'views/crm_lead_views.xml',
         'views/project_task_views.xml',
+        'views/res_company_views.xml',
     ],
     'installable': True,
 }
