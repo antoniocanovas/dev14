ce24dd84e7eab1334607bb04fa5d369ec68e5bff
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-11 09:36:53 +0200

    crm_scrap v0

--- crm_scrap/models/project_task.py
+++ crm_scrap/models/project_task.py
@@ -4,5 +4,5 @@ from odoo import _, api, fields, models
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    lead_ids = fields.Many2one('crm.lead', string="Lead", store=True)
+    lead_id = fields.Many2one('crm.lead', string="Lead", store=True)
     brand_id = fields.Many2one('product.brand', string="Brand")
