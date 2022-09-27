f7659417c126b05e06d7413418c6da857eb06b50
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-28 01:08:01 +0200

    sale_revision v0

--- sale_revision/__manifest__.py
+++ sale_revision/__manifest__.py
@@ -1,19 +1,20 @@
 {
-    'name': "Sale Order Revision All",
+    'name': "Sale Order Revision IC",
     'summary': """
-        
+        Create sale order revision with secuential code.
+        Internal tab with other revisions and chatter messages.
         """,
     'author': "Pedro Guirao",
     'license': 'AGPL-3',
     'website': "https://ingenieriacloud.com",
     'category': 'Tools',
-    'version': '14.0.5.0.0',
+    'version': '14.0.1.0.0',
     'depends': [
-        'sale_order_revision',
+        'sale_management',
     ],
     'data': [
         'views/views.xml',
     ],
     'installable': True,
-    'application': True,
+    'application': False,
 }
