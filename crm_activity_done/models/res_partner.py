45b497a42fdbfdcd4c350e2ebc36b35565b3611e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-04 16:16:38 +0200

    crm activity done

--- crm_activity_done/models/res_partner.py
+++ crm_activity_done/models/res_partner.py
@@ -7,8 +7,7 @@ class ResPartner(models.Model):
     activity_done_ids = fields.One2many('mail.message', 'author_id', store=True,
                                         domain=[('subtype_id','=',3),('message_type','=','notification')])
 
-    #def _compute_activity_done_count(self):
-    #    results = self.env['mail.activity'].search([('partner_id', '=', self.id)])
-    #    self.activity_done_count = len(results)
+    def _compute_activity_done_count(self):
+        self.activity_done_count = len(self.activity_done_ids)
 
-    activity_done_count = fields.Integer(string="'Activity's", store=False)
+    activity_done_count = fields.Integer(string="'Activity's", compute=_compute_activity_done_count ,store=False)
