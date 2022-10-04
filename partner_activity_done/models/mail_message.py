50415eac81afc819747ceded5f9aef7a22e620c1
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-05 01:49:30 +0200

    partner_activity_done v0

--- partner_activity_done/models/mail_message.py
+++ partner_activity_done/models/mail_message.py
@@ -7,11 +7,11 @@ class MailMessage(models.Model):
     @api.depends('create_date')
     def get_mail_message_name(self):
         for record in self:
-            model_name, item_name, name = "", "", ""
+            name = ""
             model_id = self.env['ir.model'].search([('model', '=', record.model)])
             item_id = self.env[record.model].search([('id', '=', record.res_id)])
-            if model_id.name: model_name = item_id.name
-            if item_id.name: item_name = item_id.name
-            if model_id.name or item_id.name: name = model_name + " => " + item_name
+            if (model_id.name): name += model_id.name
+            if (model_id.name and item_id.name): name += " => "
+            if item_id.name: name += item_id.name
             record['name'] = name
     name = fields.Char('Name', compute='get_mail_message_name', store=True)
