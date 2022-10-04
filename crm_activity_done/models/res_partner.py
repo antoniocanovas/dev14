753a7009be67635f647fe6b0ba826dcc1c129d2a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-04 15:13:55 +0200

    crm_activity_done v0

--- crm_activity_done/models/res_partner.py
+++ crm_activity_done/models/res_partner.py
@@ -4,4 +4,5 @@ from odoo import _, api, fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    activity_done_ids = fields.One2many('mail.message', 'author_id', store=True)
+    activity_done_ids = fields.One2many('mail.message', 'author_id', store=True,
+                                        domain=[('subtype_id','=',3),('message_type','=','notification')])
