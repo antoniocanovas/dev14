c7e6316ce9e31c0b4444d562e94a9b77b3e9544d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-04 13:39:01 +0200

    crm_activity_done v0

--- crm_activity_done/models/crm_lead.py
+++ crm_activity_done/models/crm_lead.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    mail_activity_done = fields.One2many('mail.message', 'lead_id', store=True)
+    mail_activity_done_ids = fields.One2many('mail.message', 'lead_id', store=True)
