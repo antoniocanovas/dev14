f124fbb41fd9dfe2f0a7de55b06cfe817d9be92e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-05 01:04:16 +0200

    crm_activity_done v0

--- crm_activity_done/models/mail_message.py
+++ crm_activity_done/models/mail_message.py
@@ -7,9 +7,11 @@ class MailMessage(models.Model):
     @api.depends('create_date')
     def get_mail_message_name(self):
         for record in self:
+            item_name = ""
             model_id = self.env['ir.model'].search([('model', '=', record.model)])
             item_id = self.env[record.model].search([('id', '=', record.res_id)])
-            record['name'] = model_id.name + " => " + item_id.name
+            if item_id.id: item_name = item_id.name
+            record['name'] = model_id.name + " => " + item_name
     name = fields.Char('Name', compute='get_mail_message_name')
 
     @api.depends('create_date')
