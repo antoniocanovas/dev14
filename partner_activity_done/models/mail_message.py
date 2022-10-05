79a340e3d2a675000337a17e6d5f5098e9233854
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-05 02:13:33 +0200

    partner_activity_done v0

--- partner_activity_done/models/mail_message.py
+++ partner_activity_done/models/mail_message.py
@@ -10,8 +10,8 @@ class MailMessage(models.Model):
             name = ""
             model_id = self.env['ir.model'].search([('model', '=', record.model)])
             item_id = self.env[record.model].search([('id', '=', record.res_id)])
-            if (model_id.name != ""): name += model_id.name
-            if (model_id.name != "" and item_id.name != ""): name += " => "
-            if (item_id.name != "" and item_id.name != ""): name += item_id.name
+            if (model_id.name): name += model_id.name
+            if (model_id.name) and (item_id.name): name += " => "
+            if (item_id.name) and (item_id.name): name += item_id.name
             record['name'] = name
     name = fields.Char(string='Name', compute='get_mail_message_name', store=False)
