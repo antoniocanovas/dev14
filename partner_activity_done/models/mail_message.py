915a7e534c20d985af9720662ea18525a4ff470a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-05 02:03:16 +0200

    partner_activity_done v0

--- partner_activity_done/models/mail_message.py
+++ partner_activity_done/models/mail_message.py
@@ -8,10 +8,11 @@ class MailMessage(models.Model):
     def get_mail_message_name(self):
         for record in self:
             name = ""
-            model_id = self.env['ir.model'].search([('model', '=', record.model)])
-            item_id = self.env[record.model].search([('id', '=', record.res_id)])
-            if (model_id.id and model_id.name): name += model_id.name
-            if (model_id.id and model_id.name and item_id.id and item_id.name): name += " => "
-            if (item_id.id and item_id.name): name += item_id.name
+            if (record.subtype_id == 3) and (record.message_type == 'notification'):
+                model_id = self.env['ir.model'].search([('model', '=', record.model)])
+                item_id = self.env[record.model].search([('id', '=', record.res_id)])
+                if (model_id.name != ""): name += model_id.name
+                if (model_id.name != "" and item_id.name != ""): name += " => "
+                if (item_id.name != "" and item_id.name != ""): name += item_id.name
             record['name'] = name
     name = fields.Char(string='Name', compute='get_mail_message_name', store=True)
