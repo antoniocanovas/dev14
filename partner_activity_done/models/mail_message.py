fe28a309d64983f43d740b4c3e64ef2f3cc18a0d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-05 02:10:36 +0200

    partner_activity_done v0

--- partner_activity_done/models/mail_message.py
+++ partner_activity_done/models/mail_message.py
@@ -8,11 +8,10 @@ class MailMessage(models.Model):
     def get_mail_message_name(self):
         for record in self:
             name = ""
-            if (record.subtype_id == 3):
-                model_id = self.env['ir.model'].search([('model', '=', record.model)])
-                item_id = self.env[record.model].search([('id', '=', record.res_id)])
-                if (model_id.name != ""): name += model_id.name
-                if (model_id.name != "" and item_id.name != ""): name += " => "
-                if (item_id.name != "" and item_id.name != ""): name += item_id.name
+            model_id = self.env['ir.model'].search([('model', '=', record.model)])
+            item_id = self.env[record.model].search([('id', '=', record.res_id)])
+            if (model_id.name != ""): name += model_id.name
+            if (model_id.name != "" and item_id.name != ""): name += " => "
+            if (item_id.name != "" and item_id.name != ""): name += item_id.name
             record['name'] = name
-    name = fields.Char(string='Name', compute='get_mail_message_name', store=False)
+name = fields.Char(string='Name', compute='get_mail_message_name', store=False)
