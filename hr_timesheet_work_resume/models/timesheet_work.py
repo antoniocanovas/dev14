e3c93e39b8ef82ad00592f9b5260ad5899507fbb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-08 13:55:06 +0200

    hr_work_resume v0

--- hr_timesheet_work_resume/models/timesheet_work.py
+++ hr_timesheet_work_resume/models/timesheet_work.py
@@ -6,3 +6,11 @@ _logger = logging.getLogger(__name__)
 class TimesheetWork(models.Model):
     _name = 'timesheet.work'
 
+    sale_order_ids = fields.Many2many('sale.order', string='Sale Orders')
+    todo_ids = fields.One2many('timesheet.line.todo', 'work_id')
+
+    @api.depends('todo_ids')
+    def get_todo_count(self):
+        for record in self:
+            record.todo_count = len(record.todo_ids.ids)
+    todo_count = fields.Integer(string='To-do', store=False, compute='get_todo_count',)
