ee69527f504ac30876408532c7b17ed4622f2653
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-08 13:33:40 +0200

    hr_work_resume v0

--- hr_timesheet_work_resume/models/__init__.py
+++ hr_timesheet_work_resume/models/__init__.py
@@ -1,4 +1,4 @@
-#from . import timesheet_line_todo
-#from . import timesheet_line_done
+from . import timesheet_line_todo
+from . import timesheet_line_done
 from . import sale_order
-#from . import timesheet_work
+from . import timesheet_work
