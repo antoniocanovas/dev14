eaa033b210aa8544a4460f1c3ff09e0189241384
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-08 14:30:39 +0200

    hr_work_resume v0

--- hr_timesheet_work_resume/models/__init__.py
+++ hr_timesheet_work_resume/models/__init__.py
@@ -1,4 +1,3 @@
-from . import sale_order
 from . import timesheet_line_todo
 from . import timesheet_line_done
 #from . import timesheet_work
--- hr_timesheet_work_resume/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-# Copyright
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from odoo import fields, models, api
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    work_id = fields.Many2one('timesheet.work', string='Work')
