1cb3408232d33d0059edb7c9752a058988fc6e20
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-08 13:51:56 +0200

    hr_work_resume v0

--- hr_timesheet_work_resume/models/timesheet_work.py
+++ hr_timesheet_work_resume/models/timesheet_work.py
@@ -1,17 +1,16 @@
-from odoo import _, api, fields, models
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import logging
-_logger = logging.getLogger(__name__)
-
+from odoo import fields, models, api
 
 class TimesheetWork(models.Model):
     _name = 'timesheet.work'
 
     sale_order_ids = fields.Many2many('sale.order', string='Sale Orders')
-#    todo_ids = fields.One2many('timesheet.line.do', 'work_id')
+    todo_ids = fields.One2many('timesheet.line.todo', 'work_id')
 
-#    @api.depends('todo_ids')
-#    def get_todo_count(self):
-#        for record in self:
-#            record.todo_count = len(record.todo_ids.ids)
-#    todo_count = fields.Integer(string='To-do', store=False, compute='get_todo_count',)
+    @api.depends('todo_ids')
+    def get_todo_count(self):
+        for record in self:
+            record.todo_count = len(record.todo_ids.ids)
+    todo_count = fields.Integer(string='To-do', store=False, compute='get_todo_count',)
