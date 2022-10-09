e461e75050ebd572f41e9fe6d3df5dd79ae85567
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-09 02:52:01 +0200

    tswork v5

--- hr_timesheet_work/models/sale_order.py
+++ hr_timesheet_work/models/sale_order.py
@@ -14,4 +14,4 @@ class SaleOrder(models.Model):
     def get_timesheet_done_count(self):
         for record in self:
             record.timesheet_done_count = len(record.timesheet_done_ids)
-    timesheet_done_count = fields.Ingeger(string='Lines Done', store=False, compute='get_timesheet_line_done_count')
+    timesheet_done_count = fields.Integer(string='Lines Done', store=False, compute='get_timesheet_line_done_count')
