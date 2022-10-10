734728d0bfe0eaa05f43459705996a2136922213
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-10 15:10:27 +0200

    work_extended v5

--- hr_timesheet_work/models/timesheet_line_todo.py
+++ hr_timesheet_work/models/timesheet_line_todo.py
@@ -30,8 +30,8 @@ class TimesheetLineTodo(models.Model):
             if record.sale_line_id.id:
                 product = record.sale_line_id.product_id.id
             record.product_id = product
-    product_id = fields.Many2one('product.product', string='Product', required=True, readonly=False,
-                                 compute='get_update_product')
+    product_id = fields.Many2one('product.product', string='Product', required=True, readonly=False,)
+#                                 compute='get_update_product')
 
     @api.depends('sale_line_id','product_id')
     def get_todo_name(self):
