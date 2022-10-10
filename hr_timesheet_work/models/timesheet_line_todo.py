e8dd20d2aa075c478170d054e5e3fc2715d9143f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-10 16:56:32 +0200

    work_extended v5

--- hr_timesheet_work/models/timesheet_line_todo.py
+++ hr_timesheet_work/models/timesheet_line_todo.py
@@ -11,32 +11,25 @@ class TimesheetLineTodo(models.Model):
     _name = "timesheet.line.todo"
     _description = "Timesheet Line To-Do"
 
+    name = fields.Char(string='Name', store=True)
     active = fields.Boolean('Active',default=True)
     work_id = fields.Many2one('timesheet.work', store=True, string='Work')
     sale_line_id = fields.Many2one('sale.order.line', store=True, string='Sale Line')
     sale_id = fields.Many2one('sale.order', related='sale_line_id.order_id', store=True)
     sale_order_ids = fields.Many2many('sale.order',related='work_id.sale_order_ids', store=False)
     product_id = fields.Many2one('product.product', string='Product', required=True, readonly=False)
-
-    @api.depends('product_id')
-    def get_todo_uom(self):
-        for record in self:
-            record.uom_id = record.product_id.uom_id.id
-    uom_id = fields.Many2one('uom.uom', string='UOM', compute='get_todo_uom', store=True)
+    uom_id = fields.Many2one('uom.uom', string='UOM', store=True)
 
     # It will be executed from AA, it can't be compute because always null on save line:
-    def get_update_work_todo_product(self):
+    def get_update_work_todo_line(self):
         for record in self:
+            name = ""
+            if record.product_id.id:
+                product = record.product_id
+                name = record.product_id.name
             if record.sale_line_id.id:
-                record.write({'product_id':record.sale_line_id.product_id.id, 'name':record.sale_line_id.name})
+                product = record.sale_line_id.product_id
+                name = record.sale_line_id.name
+            record.write({'product_id':product.id, 'name':name, 'uom_id':product.uom_id.id })
     # - - - - - -
-
-    @api.depends('sale_line_id','product_id')
-    def get_todo_name(self):
-        for record in self:
-            if record.sale_line_id.id:
-                record.name = record.sale_line_id.name
-            else:
-                record.name = record.product_id.name
-    name = fields.Char(string='Name', compute='get_todo_name', readonly=False, store=True)
 
