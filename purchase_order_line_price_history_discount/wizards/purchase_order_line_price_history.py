450b3b6ff18144e9a8b380b36679572b3ab7a891
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-24 18:10:25 +0200

    purchase history discount

--- purchase_order_line_price_history_discount/wizards/purchase_order_line_price_history.py
+++ purchase_order_line_price_history_discount/wizards/purchase_order_line_price_history.py
@@ -10,11 +10,11 @@ class PurchaseOrderLinePriceHistoryLine(models.TransientModel):
         related='purchase_order_line_id.discount'
     )
 
-    @api.depends('price_unit', 'discount')
-    def get_purchase_net_price(self):
-        self.price_net = self.price_unit * (1 - self.discount/100)
+#    @api.depends('price_unit', 'discount')
+#    def get_purchase_net_price(self):
+#        self.price_net = self.price_unit * (1 - self.discount/100)
     price_net = fields.Float(
         string='Net price',
-        store=False,
-        compute='get_purchase_net_price',
+#        store=False,
+#        compute='get_purchase_net_price',
     )
