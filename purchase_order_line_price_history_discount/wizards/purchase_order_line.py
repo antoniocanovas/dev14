2a078357ad9d2c59819126937539b15548017884
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-24 18:34:20 +0200

    purchase history discount

---
+++ purchase_order_line_price_history_discount/wizards/purchase_order_line.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+class PurchaseOrderLine(models.TransientModel):
+    _inherit = "purchase.order.line"
+
+    @api.depends('price_unit', 'price_subtotal')
+    def get_purchase_net_price(self):
+        price = 0
+        monetary_precision = self.env['decimal.precision'].sudo().search([('id', '=', 1)]).digits
+        if self.price_unit != 0:
+            price = round(self.price_subtotal / self.product_qty, monetary_precision)
+        self.write.price_net = price
+    price_net = fields.Float(
+        string='Net price',
+        store=True,
+        compute = 'get_purchase_net_price'
+    )
--- purchase_order_line_price_history_discount/wizards/purchase_order_line_price_history.py
+++ purchase_order_line_price_history_discount/wizards/purchase_order_line_price_history.py
@@ -7,18 +7,12 @@ class PurchaseOrderLinePriceHistoryLine(models.TransientModel):
 
     discount = fields.Float(
         string='Discount',
+        store=False,
         related='purchase_order_line_id.discount'
     )
 
-#    @api.depends('price_unit', 'discount')
-#    def get_purchase_net_price(self):
-#        if self.discount:
-#            self.price_net = self.price_unit
-#            self.price_net = self.purchase_order_line_id.price_unit * (1 - self.purchase_order_line_id.discount/100)
-#        else:
-#            self.price_net = self.price_unit
     price_net = fields.Float(
         string='Net price',
         store=False,
-        related = 'purchase_order_line_id.x_price_net'
+        related = 'purchase_order_line_id.price_net'
     )
