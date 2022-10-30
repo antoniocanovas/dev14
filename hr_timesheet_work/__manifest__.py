b36fe88b6741022b3748ec1cc374a197168c648b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-30 10:35:31 +0100

    hr_timesheet v10

---
+++ hr_timesheet_work/__manifest__.py
@@ -0,0 +1,37 @@
+{
+    'name': 'Timesheet Work base',
+    'version': '14.0.8.0.0',
+    'category': '',
+    'description': u"""
+
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'base_automation',
+        'purchase',
+        'stock',
+        'sale_timesheet',
+        'hr_timesheet_time_type',
+        'hr_timesheet_activity_begin_end',
+        'analytic_line_tracking',
+        'web_widget_open_tab',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/rules.xml',
+        'data/action_server.xml',
+        'views/timesheet_work_views.xml',
+        'views/work_sheet_views.xml',
+        'views/account_analytic_tag_views.xml',
+        'views/account_analytic_line_views.xml',
+        'views/stock_picking_views.xml',
+        'views/templates.xml',
+        'views/work_sheet_report.xml',
+        'views/hr_employee.xml',
+        'views/timesheet_line_todo_views.xml',
+        'views/timesheet_line_done_views.xml',
+        'views/sale_order_views.xml',
+        'views/menu_views.xml',
+    ],
+    'installable': True,
+}
--- hr_timesheet_work/models/timesheet_line_todo.py
+++ hr_timesheet_work/models/timesheet_line_todo.py
@@ -11,43 +11,25 @@ class TimesheetLineTodo(models.Model):
     _name = "timesheet.line.todo"
     _description = "Timesheet Line To-Do"
 
+    name = fields.Char(string='Name', store=True, required=True)
     active = fields.Boolean('Active',default=True)
     work_id = fields.Many2one('timesheet.work', store=True, string='Work')
     sale_line_id = fields.Many2one('sale.order.line', store=True, string='Sale Line')
     sale_id = fields.Many2one('sale.order', related='sale_line_id.order_id', store=True)
     sale_order_ids = fields.Many2many('sale.order',related='work_id.sale_order_ids', store=False)
+    product_id = fields.Many2one('product.product', string='Product', required=True, readonly=False)
+    uom_id = fields.Many2one('uom.uom', string='UOM', store=True)
 
-    #@api.onchange('product_id')
-    #def get_todo_uom(self):
-    #    for record in self:
-    #        record.uom_id = record.product_id.uom_id.id
-    uom_id = fields.Many2one('uom.uom', string='UOM',  store=True)
-
-
-    #@api.onchange('sale_line_id','product_id')
-    #def get_todo_name(self):
-    #    for record in self:
-    #        print("record todo")
-    #        if record.sale_line_id.id:
-    #            record.name = record.sale_line_id.name
-    #        else:
-    #            record.name = record.product_id.name
-    name = fields.Char(string='Name', readonly=False, store=True)
-
-    @api.onchange('product_id')
-    def get_update_product(self):
+    # It will be executed from AA, it can't be compute because always null on save line:
+    def get_update_work_todo_line(self):
         for record in self:
-            print("DEBUG")
-            print("record",record.product_id)
-            product = record.product_id.id
-            print("PRODUCT",product)
+            if (record.product_id.id):
+                record.write({'product_id': record.product_id.id,
+                              'name':record.product_id.name,
+                              'uom_id':record.product_id.uom_id.id})
             if record.sale_line_id.id:
-                print("ENTRA IF")
-                product = record.sale_line_id.product_id.id
-                print("PRODUCT", product)
-
-            record.product_id = product
-    product_id = fields.Many2one('product.product', string='Product', required=True, readonly=False,
-                                 compute='get_update_product')
-
+                record.write({'product_id': record.sale_line_id.product_id.id,
+                              'name':record.sale_line_id.name,
+                              'uom_id': record.sale_line_id.product_uom.id})
+    # - - - - - -
 
