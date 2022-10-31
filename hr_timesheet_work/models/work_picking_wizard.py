2c7bd5a3f918febe5f6c0cb2c528e29c9ed40747
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-10-31 10:53:12 +0100

    hr_timesheet v10

--- hr_timesheet_work/models/work_picking_wizard.py
+++ hr_timesheet_work/models/work_picking_wizard.py
@@ -7,10 +7,9 @@
 from odoo import api, fields, models, _
 
 
-class WorkPickingWizard(models.Model):
+class WorkPickingWizard(models.TransientModel):
     _name = "work.picking.wizard"
     _description = "Work Sheet Picking Wizard"
-    _transient = True
 
     work_sheet_id = fields.Many2one('work.sheet', string='Sheet', store=True)
     picking_ids = fields.Many2many('stock.picking', related='work_sheet_id.order_picking_ids')
