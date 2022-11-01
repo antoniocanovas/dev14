cf15c668df72162586fbc0e44b5b1c4155648287
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-11-01 01:07:29 +0100

    hr_timesheet v10

--- hr_timesheet_work/models/work_timesheet_wizard.py
+++ hr_timesheet_work/models/work_timesheet_wizard.py
@@ -25,3 +25,43 @@ class WorkTimesheetWizard(models.TransientModel):
 
     timesheet_ids = fields.Many2many('account.analytic.line')
 
+    def create_lot_worksheet_services(self):
+        # Check required fields:
+        for record in self:
+            # Required start to concatenate later, required duration to change later if startstop:
+            start = ""
+            duration = record.duration
+
+            # Chek task assigned:
+            if (record.task_id.id == False):
+                raise ValidationError('Please, assign the task you have been working.')
+
+            # Chek time consumed:
+            if (record.set_start_stop == False) and (record.duration == 0):
+                raise ValidationError('Please, set the time consumed in Duration.')
+            elif (record.set_start_stop == True) and ((record.stop - record.start) <= 0):
+                raise ValidationError('Please review start & stop time consumed.')
+
+            # CASE USER NOT ADMINISTRATOR, CAN'T SEE FIELD employee_ids => Self timesheet:
+            if record.employee_ids.ids:
+                employee_ids = record.employee_ids
+            else:
+                employee_ids = [self.env.user.employee_id]
+
+            # CASE PROJECT:
+            if (record.work_id.type == "project") and (record.project_id.id):
+                for li in employee_ids:
+                    if not li.user_id:
+                        raise ValidationError('Empleado sin usuario asignado, revisa su ficha de empleado')
+                    new = self.env['account.analytic.line'].create(
+                        {'work_sheet_id': record.id, 'name': record.name, 'project_id': record.project_id.id,
+                         'task_id': record.task_id.id, 'date': record.work_sheet_id.date,
+                         'account_id': record.work_sheet_id.project_analytic_id.id,
+                         'company_id': record.work_sheet_id.company_id.id,
+                         'tag_ids': [(6,0,record.analytic_tag_ids.ids)],
+                         'employee_id': li.id, 'unit_amount': duration, 'time_type_id': record.time_type_id.id,
+                         'user_id':li.user_id.id
+                         })
+                    if (record.set_start_stop == True):
+                        duration = record.stop - record.start
+                        new.write({'time_start':record.start, 'time_stop':record.stop, 'unit_amount':duration})
