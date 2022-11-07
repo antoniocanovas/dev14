1ff6882d92c40f6a3cdd30e7d1554d81c061a262
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-11-07 18:17:13 +0100

    wup discount v5

---
+++ sale_wup/models/wup_sol_wizard.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    Serincloud SL
+##############################################################################
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class WupSolWizard(models.TransientModel):
+    _name = "wup.sol.wizard"
+    _description = "WUP New Sale Order Line add Wizard"
+
+    picking_ids = fields.Many2many('stock.picking', related='work_sheet_id.order_picking_ids')
+    picking_selection_ids = fields.Many2many('stock.picking', string="Selecteds")
+
+    def assign_work_sheet(self):
+        for sp in self.picking_selection_ids:
+            sp['work_sheet_id'] = self.work_sheet_id.id
+
+
+@api.depends('version')
+    def get_work_sheet_timesheets(self):
+        self.timesheet_ids = [(6,0,self.work_sheet_id.project_service_ids.ids)]
+    timesheet_ids = fields.Many2many('account.analytic.line', store=True, readonly=False,
+                                     compute="get_work_sheet_timesheets")
+
+    def create_lot_worksheet_services(self):
+        # Check required fields:
+        for record in self:
+            # Required start to concatenate later, required duration to change later if startstop:
+            start = ""
+            duration = record.duration
+
+            # Chek task assigned:
+            if (record.task_id.id == False):
+                raise ValidationError('Please, assign the task you have been working.')
+
+            # Chek time consumed:
+            if (record.set_start_stop == False) and (record.duration == 0):
+                raise ValidationError('Please, set the time consumed in Duration.')
+            elif (record.set_start_stop == True) and ((record.stop - record.start) <= 0):
+                raise ValidationError('Please review start & stop time consumed.')
+
+            # CASE USER NOT ADMINISTRATOR, CAN'T SEE FIELD employee_ids => Self timesheet:
+            if record.employee_ids.ids:
+                employee_ids = record.employee_ids
+            else:
+                employee_ids = [self.env.user.employee_id]
+
+            # CASE PROJECT:
+            if (record.work_sheet_id.work_id.type == "project") and (record.project_id.id):
+                for li in employee_ids:
+                    if not li.user_id:
+                        raise ValidationError('Empleado sin usuario asignado, revisa su ficha de empleado')
+                    new = self.env['account.analytic.line'].create(
+                        {'work_sheet_id': record.work_sheet_id.id, 'name': record.name,
+                         'project_id': record.project_id.id,
+                         'task_id': record.task_id.id, 'date': record.date,
+                         'account_id': record.work_sheet_id.project_analytic_id.id,
+                         'company_id': record.work_sheet_id.company_id.id,
+                         'tag_ids': [(6,0,record.analytic_tag_ids.ids)],
+                         'employee_id': li.id, 'unit_amount': duration, 'time_type_id': record.time_type_id.id,
+                         'user_id':li.user_id.id
+                         })
+                    if (record.set_start_stop == True):
+                        duration = record.stop - record.start
+                        new.write({'time_start':record.start, 'time_stop':record.stop, 'unit_amount':duration})
+                    record['version'] = record.version + 1
+        return {
+            'name': 'Work Sheet Add Timesheet wizard view',
+            'view_type': 'tree',
+            'view_mode': 'form',
+            'res_model': 'work.timesheet.wizard',
+            'type': 'ir.actions.act_window',
+            'view_id':
+                self.env.ref('hr_timesheet_work.work_timesheet_wizard_default_form').id,
+            'context': dict(self.env.context),
+            'target': 'new',
+            'res_id': self.id,
+        }
+
+
