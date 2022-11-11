b361c1c04d2e720fe7e25d3b021a38801b20f7b2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-11-11 17:51:51 +0100

    pp sequence v0

---
+++ pos_product_sequence/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'POS product sequence',
+    'version': '14.0.1.0.0',
+    'category': '',
+    'description': u"""
+    Product sequence field in product view
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
