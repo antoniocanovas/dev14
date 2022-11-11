1a929eca9648907fa80fe01c5e7ba3d0bb091456
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-11-11 18:03:11 +0100

    pp sequence v0

--- pos_product_sequence/__manifest__.py
+++ pos_product_sequence/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '14.0.1.0.0',
     'category': '',
     'description': u"""
-    Product sequence field in product view
+    Product sequence field in product view, change this value to set product order view in pos environment.
 """,
     'author': 'Serincloud',
     'depends': [
