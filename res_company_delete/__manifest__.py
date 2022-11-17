fb8cf6d5d8bc1f5d7848d81bb1e9c537e14297a9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-11-17 16:00:24 +0100

    Delete company

---
+++ res_company_delete/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Company Delete',
+    'version': '14.0.1.1.0',
+    'category': '',
+    'description': u"""
+""",
+    'author': 'Serincloud',
+    'depends': [
+
+    ],
+    'data': [
+        'data/delete_company.xml',
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+}
