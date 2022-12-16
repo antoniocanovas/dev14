f0f6a58acc2f74a34a4d38881a766a41ffc8a496
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-16 11:53:01 +0100

    hr_expense_detail v0

--- hr_expense_detail/models/hr_expense.py
+++ hr_expense_detail/models/hr_expense.py
@@ -13,3 +13,14 @@ class HrExpense(models.Model):
             total += li.amount
         self.line_amount = total
     line_amount = fields.Float('Detail total', store=False, compute='get_lines_amount')
+
+    @api.depends('line_ids.amount', 'line_ids')
+    def get_lines_amount_estimation(self):
+        total = 0
+        for li in self.line_ids:
+            if li.type_id.amount > li.amount:
+                total += li.type_id.amount
+            else:
+                total += li.amount
+        self.market_amount = total
+    market_amount = fields.Float('Detail market', store=False, compute='get_lines_amount_estimation')
