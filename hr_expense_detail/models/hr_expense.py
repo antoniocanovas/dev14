f24c0249dc10392482d4fb55b2f4f02fd8b0658a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-16 11:26:21 +0100

    hr_expense_detail v0

--- hr_expense_detail/models/hr_expense.py
+++ hr_expense_detail/models/hr_expense.py
@@ -4,7 +4,7 @@ from odoo import _, api, fields, models
 class HrExpense(models.Model):
     _inherit = 'hr.expense'
 
-    line_ids = fields.One2many('hr.expense.line', 'expense_id', store=True)
+    line_ids = fields.One2many('hr.expense.line', 'expense_id', string="Detail", store=True)
 
     @api.depends('line_ids.amount', 'line_ids')
     def get_lines_amount(self):
