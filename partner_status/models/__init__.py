2dfe1aabe00f149e529c404e7bff15a986a30d06
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-27 18:42:15 +0100

    partner_plan v0

--- partner_status/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import crm_lead
-from . import partner_status
-from . import res_partner
-
--- partner_status/models/crm_lead.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    partner_status = fields.Many2one('partner.status', related='partner_id.status', store=True)
-    is_prospection = fields.Boolean('Prospection', related='partner_status.is_prospection')
-
