4cc24e8daf73f09d4b8ab5e78cef1dfd5518265e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-12-27 17:16:43 +0100

    partner_status v0

--- partner_status/models/crm_lead.py
+++ partner_status/models/crm_lead.py
@@ -5,5 +5,5 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     partner_status = fields.Many2one('partner.status', related='partner_id.status', store=True)
-    is_prospection = fields.Boolean('Prospection', related='status.is_prospection')
+    is_prospection = fields.Boolean('Prospection', related='partner_status.is_prospection')
 
