89edbffd9c32aaabcc5879719ee23defc56e3e52
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-04 12:21:39 +0100

    scrap unbuild wizard v1

---
+++ scrap_unbuild/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+
+{
+    "name": "Scrap Unbuild",
+    "version": "14.0.2.0.0",
+    "category": "Sales",
+    "author": "www.serincloud.com",
+    "maintainer": "Pedroguirao",
+    "website": "www.serincloud.com",
+    "license": "AGPL-3",
+    "depends": [
+        'purchase',
+        'sale_management',
+        'stock',
+        'account',
+        'analytic',
+        'project',
+        'hr_timesheet',
+        'base_automation',
+        'product_analytic',
+        'product_chassis',
+    ],
+    "data": [
+        "views/product.xml",
+        "views/menu_views.xml",
+        "views/res_company.xml",
+        "data/action_server.xml",
+        "data/action_server_wizard.xml",
+        "data/sequence.xml",
+        "security/ir.model.access.csv",
+        'views/scrap_unbuild_wizard.xml',
+        'views/product_template_views.xml',
+    ],
+    "installable": True,
+}
--- scrap_unbuild_wizard/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import scrap_unbuild_wizard
-from . import product_template
-
-
