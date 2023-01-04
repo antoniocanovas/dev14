e74a4013c355a709e0ec2a27298ca6324c4c2ee8
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-04 14:20:06 +0100

    scrap unbuild wizard v1

--- scrap_unbuild/models/scrap_unbuild_wizard.py
+++ scrap_unbuild/models/scrap_unbuild_wizard.py
@@ -11,8 +11,8 @@ class ScrapUnbuildWizard(models.TransientModel):
         if self.product_tmpl_id.id: name += " " + self.product_tmpl_id.name
         if self.unbuild_set_id.id: name += " " + self.unbuild_set_id.name
         self.name = name
+    name = fields.Char('Name', compute='get_unbuild_wizard_name', readonly=False)
 
-    name = fields.Char('Name', compute='get_unbuild_wizard_name', readonly=True)
     product_tmpl_id = fields.Many2one('product.template', string='Product')
     unbuild_set_id = fields.Many2one('unbuild.set', string='SET')
     inventory_id = fields.Many2one('stock.inventory', string='Inventory', readonly=True)
