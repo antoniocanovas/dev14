a350732dcb163e2a14f2dd9b595db8114ae2be06
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-05 16:58:27 +0100

    -crm scrap

--- crm_scrap/models/crm_lead.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    task_ids = fields.One2many('project.task', 'lead_id', store=True)
-    scrap_project_id = fields.Many2one('project.project', store=False, related='company_id.scrap_project_id')
-    scrap_user_id = fields.Many2one('res.users', store=False, related='company_id.scrap_user_id')
--- crm_scrap/models/project_task.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-
-class ProjectTask(models.Model):
-    _inherit = 'project.task'
-
-    lead_id = fields.Many2one('crm.lead', string="Lead", store=True)
-    brand_id = fields.Many2one('product.brand', string="Brand")
-    categ_id = fields.Many2one('scrap.category', string="Category")
