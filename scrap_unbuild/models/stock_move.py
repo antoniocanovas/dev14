32d616e22f49f901e0886a193f77b8b3841d953e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-13 00:34:16 +0100

    scrap unbuild v3

--- scrap_unbuild/models/stock_move.py
+++ scrap_unbuild/models/stock_move.py
@@ -10,12 +10,11 @@ from odoo import api, fields, models, _
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    @api.depends('product_id.product_tmpl_id.parent_id', 'inventory_id.unbuild_product_tmpl_id')
+#    @api.depends('product_id.product_tmpl_id.parent_id', 'inventory_id.unbuild_product_tmpl_id')
+    @api.depends('inventory_id')
     def get_unbuild_product_tmpl_id(self):
-        if self.inventory_id.unbuild_product_tmpl_id.id:
-            parent = self.inventory_id.unbuild_product_tmpl_id
-        else:
-           parent = self.product_id.product_tmpl_id.parent_id
-        self.unbuild_product_tmpl_id = parent.id
+        if self.inventory_id.id:
+            self.unbuild_product_tmpl_id = self.product_id.product_tmpl_id.parent_id.id
     unbuild_product_tmpl_id = fields.Many2one('product.template', string='Unbuild Parent', store=True, readonly=True,
-           compute='get_unbuild_product_tmpl_id')
+                                                         compute='get_unbuild_product_tmpl_id'
+                                              )
