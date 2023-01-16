6be2b4663b8b8a9280a8abfccd7ba69a815f7102
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-16 09:56:28 +0100

    scrap unbuild v3

---
+++ scrap_unbuild/models/unbuild_part.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+from odoo import api, fields, models, _
+
+
+class UnbuildPart(models.Model):
+    _name = "unbuild.part"
+    _description = "Parts of scrap product"
+
+    name = fields.Char(string='Name')
+    category_id = fields.Many2one(
+        'product.category',
+        string='Category',
+        required=True,
+    )
+    product_id = fields.Many2one('product.product',string='Product',
+                                      help='Si asignas un producto, al despiezar se añadirá la cantidad al stock.'
+                                           'Se utilizarán sus fotos y precio de este producto sin distinción con otras piezas similares.')
