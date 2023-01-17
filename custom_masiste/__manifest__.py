c703cb744c1202b364f28e5058790081cba06279
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-17 17:53:42 +0100

    custom masiste product views

--- custom_masiste/__manifest__.py
+++ custom_masiste/__manifest__.py
@@ -19,6 +19,7 @@
         "views/sale_order_view.xml",
         "views/helpdesk_ticket_view.xml",
         "views/res_partner.xml",
+        "views/product_view.xml",
         "data/action_server.xml",
     ],
     "installable": True,
