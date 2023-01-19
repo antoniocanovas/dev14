2c252fc039ae4293a8ac41aab11bee6706d6d7b4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-01-19 18:23:47 +0100

    sale_trace v0

--- sale_analytic_trace/models/sale_order.py
+++ sale_analytic_trace/models/sale_order.py
@@ -11,4 +11,12 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    trace_line_id = fields.Many2one('trace.line', string='Tracing')
+    @api.depends('product_id')
+    def _get_trace_line(self):
+        for record in self:
+            trace_line = env['trace.line'].search([('sale_id','=',record.order_id.id),('product_id','=',record.product_id.id)])
+            if not trace_line.id:
+                trace_line = env['trace.line'].create({'sale_id':record.order_id.id, 'product_id':record.product_id.id})
+            record['trace_line_id'] = trace_line.id
+    trace_line_id = fields.Many2one('trace.line', string='Tracing', compute='_get_trace_line')
+
