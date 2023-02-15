6c120ea45dd0f2bc40dd11917c94050f7e8f4bfa
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-15 13:55:21 +0100

    external_work

--- external_work/__manifest__.py
+++ external_work/__manifest__.py
@@ -8,12 +8,15 @@ External Work
     'author': 'Serincloud',
     'depends': [
         'sale_timesheet',
+        'hr_expense',
     ],
     'data': [
-#        'data/action_server.xml',
+#       'data/action_server.xml',
         'security/ir.model.access.csv',
         'views/external_work_views.xml',
         'views/menu_views.xml',
+        'views/external_work_report.xml',
+        'views/templates.xml',
     ],
     'installable': True,
 }
