aac179f197f1ff1482ec1360d8a789d5e822e317
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-16 11:03:03 +0100

    external work v0

--- external_work/models/external_work_line.py
+++ external_work/models/external_work_line.py
@@ -22,7 +22,8 @@ class ExternalWork(models.Model):
             if name: name += " - "
             if self.product_id.id: name += self.product_id.name
             record['name'] = name
-    name = fields.Char('Name', compute='_get_workline_name')
+#    name = fields.Char('Name', compute='_get_workline_name')
+    name = fields.Char('Name')
 
     type        = fields.Selection(selection=TYPE, string="Type", default=TYPE[0][0])
 
