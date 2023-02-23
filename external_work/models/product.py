3cdcc029f260403f66bc95eca93097581c78b596
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-02-23 01:51:33 +0100

    external work v0

--- external_work/models/product.py
+++ external_work/models/product.py
@@ -9,7 +9,7 @@ class ProductProduct(models.Model):
     @api.depends('type','uom_id')
     def _get_is_work_time(self):
         is_work_time = False
-        wtime_category = self.env['model.date'].search([('model','=','uom.category'),('name','=','uom_categ_wtime')])
+        wtime_category = self.env['model.data'].search([('model','=','uom.category'),('name','=','uom_categ_wtime')])
         if (self.uom_id.category_id.id == wtime_category.res_id): is_work_time = True
         self.is_work_time = is_work_time
     is_work_time = fields.Boolean('Is working time', store=True, compute=_get_is_work_time)
