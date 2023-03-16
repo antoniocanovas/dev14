d3678340921420db51d619f6d20d6c6d0e026b90
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-17 00:17:11 +0100

    external rev1

--- external_work/models/external_work.py
+++ external_work/models/external_work.py
@@ -23,7 +23,7 @@ class ExternalWork(models.Model):
     def _get_default_employee(self):
         self.employee_id = self.user.employee_id.id
     employee_id = fields.Many2one('hr.employee', string="Employee",
-                                  default=lambda self: self.env.user.employee_id.id,
+#                                  default=lambda self: self.env.user.employee_id.id,
                                   default=_get_default_employee,
                                   )
 
