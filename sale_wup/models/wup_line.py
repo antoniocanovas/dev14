0b76b5bb442781650e51152ce4fa4e42fccfe007
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-03-21 18:37:02 +0100

    +wup

--- sale_wup/models/wup_line.py
+++ sale_wup/models/wup_line.py
@@ -19,6 +19,7 @@ class WupLine(models.Model):
     effective_hours = fields.Float(string="Eff. Hours", related='task_id.effective_hours', store=False)
     #Modificado 09/09/2022 por formato de impresión sin respetar formato, puede hacerse calculado para pasar a char.
     sale_line_name = fields.Text(string='Sale line', related='sale_line_id.name')
+    service_tracking = fields.Selection('Service tracking', store=True, related='product_id.service_tracking')
 
     @api.depends('sale_line_id.product_uom_qty', 'product_uom_qty')
     def get_wup_product_sale_qty(self):
