7842c4a66e986500528df9d33de559b3ee266a31
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-16 16:36:15 +0200

    salaanalytic v0

--- invoce_sale_analytic_cost/models/account_move_line.py
+++ invoce_sale_analytic_cost/models/account_move_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    analytic_cost_id = fields.Many2one('account.analytic.line', 'Analytic cost')
+    analytic_cost_id = fields.Many2one('account.analytic.line', string='Analytic cost', store=True)
