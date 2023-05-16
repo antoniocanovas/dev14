c9a561fe77c3d2109ed5edd8e9841b30b1f8a183
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-16 02:48:07 +0200

    invoice sale cost auto analytic

--- invoce_sale_analytic_cost/models/account_move.py
+++ invoce_sale_analytic_cost/models/account_move.py
@@ -9,11 +9,11 @@ class AccountMove(models.Model):
     def _create_analytic_line(self):
         if self.move_type in ['out_invoice']:
             for li in self.invoice_line_ids:
-                if (li.account_analytic_id.id) and not (li.analytic_cost_id.id) \
+                if (li.analytic_account_id.id) and not (li.analytic_cost_id.id) \
                         and (li.product_id.product_tmpl_id.autoanalytic) and (self.state in ['posted']):
                     new = self.env['account.analytic.line'].create({
                         'name':li.product_id.name,
-                        'account_id': li.account_analytic_id.id,
+                        'account_id': li.analytic_account_id.id,
                         'product_id': li.product_id.id,
                         'unit_amount': li.quantity,
                         'product_uom_id': li.product_uom_id.id
