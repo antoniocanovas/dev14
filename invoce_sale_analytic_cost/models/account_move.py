3ba6f367d79bf3302c40075cfdd78226d49ba6da
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-16 17:43:57 +0200

    salaanalytic v0

--- invoce_sale_analytic_cost/models/account_move.py
+++ invoce_sale_analytic_cost/models/account_move.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class AccountInvoice(models.Model):
     _inherit = "account.move"
@@ -6,14 +7,27 @@ class AccountInvoice(models.Model):
 
     def _create_analytic_line(self):
         if self.move_type in ['out_invoice']:
-            for li in self.invoice_line_ids:
-                if (li.analytic_account_id.id) and not (li.analytic_cost_id.id) \
-                        and (li.product_id.product_tmpl_id.autoanalytic) and (self.state in ['posted']):
+
+            for li in record.invoice_line_ids:
+                if not (li.analytic_cost_id.id) and (li.analytic_account_id.id) and \
+                        (li.product_id.product_tmpl_id.autoanalytic) and (self.state in ['posted']):
+
+                    if li.product_uom_id.uom_type == 'reference':
+                        cost = li.product_id.standard_price
+                    elif li.product_uom_id.uom_type == 'bigger':
+                        cost = li.product_id.standard_price * li.product_uom_id.factor_inv
+                    elif li.product_uom_id.uom_type == 'smaller':
+                        cost = li.product_id.standard_price / li.product_uom_id.factor
+                    cost = - cost * li.quantity
+
                     new = self.env['account.analytic.line'].create({
                         'name':li.product_id.name,
                         'account_id': li.analytic_account_id.id,
                         'product_id': li.product_id.id,
                         'unit_amount': li.quantity,
-                        'product_uom_id': li.product_uom_id.id
+                        'product_uom_id': li.product_uom_id.id,
+                        'amount': cost
                     })
                     li['analytic_cost_id'] = new.id
+        else:
+            raise ValidationError('Opción válida para facturas de venta confirmadas.')
