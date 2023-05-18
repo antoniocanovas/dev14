515a303ac55b5a111a3c946ad12eaf6f3f78821d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-18 02:04:56 +0200

    ppupdatev2

---
+++ purchase_price_update/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+
+{
+    "name": "Purchase price update",
+    "version": "14.0.2.0.2",
+    "category": "Sales",
+    "author": "Serincloud",
+    "maintainer": "Serincloud",
+    "website": "www.serincloud.com",
+    "license": "AGPL-3",
+    "depends": [
+        'purchase',
+        'purchase_discount',
+    ],
+    "data": [
+        "views/model_view.xml",
+        "wizards/wizard_template.xml",
+        "security/ir.model.access.csv",
+        "security/user_groups.xml",
+    ],
+    "installable": True,
+}
