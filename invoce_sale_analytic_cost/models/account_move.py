366fe5df198701113689240640de4375d7919fad
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-01 19:44:50 +0200

    analyticcost

--- invoce_sale_analytic_cost/models/account_move.py
+++ invoce_sale_analytic_cost/models/account_move.py
@@ -7,18 +7,16 @@ class AccountInvoice(models.Model):
     @api.depends('state')
     def _create_analytic_line(self):
         if (self.move_type in ['out_invoice']) and (self.state in ['posted']):
-
-            for li in record.invoice_line_ids:
-                if not (li.analytic_cost_id.id) and (li.analytic_account_id.id) and \
-                        (li.product_id.product_tmpl_id.autoanalytic) and (self.state in ['posted']):
-
+            cost = 0
+            for li in self.invoice_line_ids:
+                if not (li.analytic_cost_id.id) and (li.analytic_account_id.id) and (li.product_id.product_tmpl_id.autoanalytic):
                     if li.product_uom_id.uom_type == 'reference':
                         cost = li.product_id.standard_price
                     elif li.product_uom_id.uom_type == 'bigger':
                         cost = li.product_id.standard_price * li.product_uom_id.factor_inv
                     elif li.product_uom_id.uom_type == 'smaller':
                         cost = li.product_id.standard_price / li.product_uom_id.factor
-                    cost = - cost * li.quantity
+                    cost = -1 * cost * li.quantity
 
                     new = self.env['account.analytic.line'].create({
                         'name':li.product_id.name,
@@ -29,5 +27,3 @@ class AccountInvoice(models.Model):
                         'amount': cost
                     })
                     li['analytic_cost_id'] = new.id
-        else:
-            raise ValidationError('Opción válida para facturas de venta confirmadas.')
