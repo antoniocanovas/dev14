5a5e52bed9376a17ad73d2c414c5810ab6019f02
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-02 12:02:52 +0200

    delete analytic_cost from invoice

--- invoice_sale_analytic_cost/models/account_move_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api
-
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    analytic_cost_id = fields.Many2one('account.analytic.line', string='Analytic cost', store=True)
--- invoice_sale_analytic_cost/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-import logging
-_logger = logging.getLogger(__name__)
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    autoanalytic = fields.Boolean('Auto analytic cost', default=True)
