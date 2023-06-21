efde05a4cf3e528632279d6bd02c285524c80de6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-21 17:01:25 +0200

    crm_test

--- crm_test/models/crm_lead.py
+++ crm_test/models/crm_lead.py
@@ -2,6 +2,6 @@ from odoo import _, api, fields, models
 
 
 class CRMLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = 'crm.lead2opportunity.partner'
 
     action = fields.Selection([('exist', 'Exist'), ('nothing','Noting')])
