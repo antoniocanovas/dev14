b92b476bf14f7246e83f9ab14b36c4f537c7e8c6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-21 18:10:57 +0200

    crm_test

--- crm_test/models/crm_lead.py
+++ crm_test/models/crm_lead.py
@@ -4,8 +4,14 @@ from odoo import _, api, fields, models
 class CRMLead(models.Model):
     _inherit = 'crm.lead2opportunity.partner'
 
-    action = fields.Selection([
-#        ('create', 'Create a new customer'),
-        ('exist', 'Link to an existing customer'),
-        ('nothing', 'Do not link to a customer')
-    ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
+    action = fields.Selection(selection='_get_types', string='Related Customer',
+                              compute='_compute_action', readonly=False, store=True, compute_sudo=False)
+
+    @api.model
+    def _get_types(self):
+        selection = [
+#            ('create', 'Create a new customer'),
+            ('exist', 'Link to an existing customer'),
+            ('nothing', 'Do not link to a customer')
+        ]
+        return selection
