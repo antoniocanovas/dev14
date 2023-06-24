b2b2b4e8eac6f1204f5cab24afcea61f94b423ee
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-24 11:39:47 +0200

    wup again

--- crm_test/models/sale_order_line.py
+++ crm_test/models/sale_order_line.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class Lead2opportunity(models.TransientModel):
     _inherit = 'sale.order.line'
 
-    order_line = fields.one2many('sale.order.line', 'order_id', 'Order Lines', limit=200)
+    order_line = fields.One2many('sale.order.line', 'order_id', 'Order Lines', limit=200)
