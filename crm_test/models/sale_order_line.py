3a22b1d5996162c862a53165e686b7c6738ef70c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-06-24 11:44:03 +0200

    wup again

--- crm_test/models/sale_order_line.py
+++ crm_test/models/sale_order_line.py
@@ -4,4 +4,4 @@ from odoo import _, api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    order_line = fields.One2many('sale.order.line', 'order_id', 'Order Lines', limit=200)
+    order_line = fields.One2many('sale.order.line', 'order_id', limit=200)
