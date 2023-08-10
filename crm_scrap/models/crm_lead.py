dcaef7a2d63f7ae7cbc795dbd7e7161f93b93bad
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-10 20:05:32 +0200

    all rioc

--- crm_scrap/models/crm_lead.py
+++ crm_scrap/models/crm_lead.py
@@ -13,16 +13,16 @@ class CrmLead(models.Model):
     description_tech = fields.Text('Technical notes', store=True)
 
     # Lo quito porque lo pide Cristina 9/8/23, siempre seleccionarán empresa y sólo se muestran etiquetas del primer contacto:
-    @api.depends('partner_id')
-    def _get_partner_tags(self):
-        for record in self:
-            tags = []
-            if (record.partner_id.parent_id.id) and (record.partner_id.parent_id.category_id.ids):
-                for tag in record.partner_id.parent_id.category_id: tags.append(tag.id)
-            if (record.partner_id.category_id.ids):
-                for tag in record.partner_id.category_id: tags.append(tag.id)
-            record['partner_tag_ids'] = [(6,0,tags)]
-    partner_tag_ids = fields.Many2many('res.partner.category', compute='_get_partner_tags', store=False, string='Partner Tags')
+#    @api.depends('partner_id')
+#    def _get_partner_tags(self):
+#        for record in self:
+#            tags = []
+#            if (record.partner_id.parent_id.id) and (record.partner_id.parent_id.category_id.ids):
+#                for tag in record.partner_id.parent_id.category_id: tags.append(tag.id)
+#            if (record.partner_id.category_id.ids):
+#                for tag in record.partner_id.category_id: tags.append(tag.id)
+#            record['partner_tag_ids'] = [(6,0,tags)]
+#    partner_tag_ids = fields.Many2many('res.partner.category', compute='_get_partner_tags', store=False, string='Partner Tags')
 #    partner_tag_ids = fields.Many2many('res.partner.category', related='partner_id.category_id', readonly=False, store=True, string='Partner Tags')
 
     @api.depends('partner_id')
